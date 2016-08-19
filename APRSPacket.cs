446bc4406a0a276b198b95fad4f9f05fb0a65c9c
Cameron Bielstein <ctbielstein@gmail.com>
2016-08-19 00:08:46 -0700

    Created APRSPacket object.

---
+++ APRSPacket.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace APaRSer
+{
+    /// <summary>
+    /// A representation of an APRS Packet.
+    /// Does decoding of an APRS packet as a string.
+    /// </summary>
+    public class APRSPacket
+    {
+        /// <summary>
+        /// Constructs a new APRSPacket object by decoding the encodedPacket string.
+        /// Really just a shortcut for calling the Decode function.
+        /// </summary>
+        /// <param name="encodedPacket">A string encoding of an APRS packet</param>
+        public APRSPacket(string encodedPacket)
+        {
+            Decode(encodedPacket);
+        }
+
+        /// <summary>
+        /// Takes a string encoding of an APRS packet, decodes it, and saves it in to this object.
+        /// </summary>
+        /// <param name="encodedPacket">A string encoding of an APRS packet</param>
+        public void Decode(string encodedPacket)
+        {
+            throw new NotImplementedException("APRSPacket.Decode(string)");
+        }
+    }
+}

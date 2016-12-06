8489cf843194de4480ef5d423d60676322378f1d
Cameron Bielstein <ctbielstein@gmail.com>
2016-12-06 00:27:36 -0800

    Implemented latitude and ambiguity decoding.

---
+++ APaRSerUnitTests/PositionUnitTests.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using APaRSer;
+
+namespace APaRSerUnitTests
+{
+    [TestClass]
+    public class PositionUnitTests
+    {
+        [TestMethod]
+        public void DecodeLatitudeFromSpec()
+        {
+            string latitude = "4903.50N";
+            Position p = new Position();
+            PrivateObject pp = new PrivateObject(p);
+
+            double decodedLat = (double)pp.Invoke("DecodeLatitude", latitude, 0);
+            Assert.AreEqual(49.0583, decodedLat);
+            Assert.AreEqual(0, pp.GetField("Ambiguity"));
+        }
+
+        [TestMethod]
+        public void DecodeLatitudeWithAmbiguityFromSpec1()
+        {
+            string latitude = "4903.5 N";
+            Position p = new Position();
+            PrivateObject pp = new PrivateObject(p);
+
+            double decodedLat = (double)pp.Invoke("DecodeLatitude", latitude, 0);
+            Assert.AreEqual(49.0583, decodedLat);
+            Assert.AreEqual(1, pp.GetField("Ambiguity"));
+        }
+
+        [TestMethod]
+        public void DecodeLatitudeWithAmbiguityFromSpec2()
+        {
+            string latitude = "4903.  N";
+            Position p = new Position();
+            PrivateObject pp = new PrivateObject(p);
+
+            double decodedLat = (double)pp.Invoke("DecodeLatitude", latitude, 0);
+            Assert.AreEqual(49.05, decodedLat);
+            Assert.AreEqual(2, pp.GetField("Ambiguity"));
+        }
+
+        [TestMethod]
+        public void DecodeLatitudeWithAmbiguityFromSpec3()
+        {
+            string latitude = "490 .  N";
+            Position p = new Position();
+            PrivateObject pp = new PrivateObject(p);
+
+            double decodedLat = (double)pp.Invoke("DecodeLatitude", latitude, 0);
+            Assert.AreEqual(49.0, decodedLat);
+            Assert.AreEqual(3, pp.GetField("Ambiguity"));
+        }
+
+        [TestMethod]
+        public void DecodeLatitudeWithAmbiguityFromSpec4()
+        {
+            string latitude = "49  .  N";
+            Position p = new Position();
+            PrivateObject pp = new PrivateObject(p);
+
+            double decodedLat = (double)pp.Invoke("DecodeLatitude", latitude, 0);
+            Assert.AreEqual(49.0, decodedLat);
+            Assert.AreEqual(4, pp.GetField("Ambiguity"));
+        }
+
+        [TestMethod]
+        public void CountAmbiguityInvalid()
+        {
+            string latitude = "49  .1  N";
+            Position p = new Position();
+            PrivateObject pp = new PrivateObject(p);
+
+            try
+            {
+                pp.Invoke("CountAmbiguity", latitude);
+
+                Assert.Fail("This should have thrown an exception.");
+            }
+            catch (ArgumentException)
+            {
+                return;
+            }
+
+            Assert.Fail("This should have thrown an ArgumentException.");
+        }
+
+        [TestMethod]
+        public void EnforceAmbiguityBasic()
+        {
+            string latitude = "4903.50N";
+            Position p = new Position();
+            PrivateObject pp = new PrivateObject(p);
+
+            string ambiguous = (string)pp.Invoke("EnforceAmbiguity", latitude, 2);
+            Assert.AreEqual("4903.  N", ambiguous);
+        }
+
+        [TestMethod]
+        public void EnforceAmbiguityBasic2()
+        {
+            string latitude = "4903.50N";
+            Position p = new Position();
+            PrivateObject pp = new PrivateObject(p);
+
+            string ambiguous = (string)pp.Invoke("EnforceAmbiguity", latitude, 4);
+            Assert.AreEqual("49  .  N", ambiguous);
+        }
+
+        [TestMethod]
+        public void EnforceAmbiguityInvalidArg()
+        {
+            string latitude = "4903.50N";
+            Position p = new Position();
+            PrivateObject pp = new PrivateObject(p);
+
+            try
+            {
+                string ambiguous = (string)pp.Invoke("EnforceAmbiguity", latitude, 7);
+
+                Assert.Fail("Should have thrown an exception.");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return;
+            }
+
+            Assert.Fail("Should have thrown an ArgumentOutOfRangeException.");
+        }
+    }
+}
--- Position.cs
+++ Position.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using GeoCoordinatePortable;
+using System.Text;
 
 namespace APaRSer
 {
-    class Position
+    public class Position
     {
         public GeoCoordinate Coordinates = new GeoCoordinate(0, 0);
         public char SymbolTableIdentifier = '.';
@@ -48,8 +49,9 @@ namespace APaRSer
                 throw new ArgumentException("The given APRS coordinates is " + coords.Length + " coords long instead of the expected 19: " + coords);
             }
 
-            double latitude = DecodeLatitude(coords.Substring(0, 8));
-            double longitude = DecodeLongitude(coords.Substring(9, 9));
+            Ambiguity = 0;
+            double latitude = DecodeLatitude(coords.Substring(0, 8), Ambiguity);
+            double longitude = DecodeLongitude(coords.Substring(9, 9), Ambiguity);
 
             SymbolTableIdentifier = coords[8];
             SymbolCode = coords[18];
@@ -60,22 +62,142 @@ namespace APaRSer
         /// Decode the latitude section of the coordinate string
         /// </summary>
         /// <param name="coords">APRS encoded latitude coordinates</param>
+        /// <param name="nEnforceAmbiguity">Enforce a certain level of ambiguity</param>
         /// <returns>Decimal latitude</returns>
-        private double DecodeLatitude(string coords)
+        private double DecodeLatitude(string coords, int nEnforceAmbiguity)
         {
-            // Count ambiguity
+            // Ensure latitude is well formatted
+            if (coords == null)
+            {
+                throw new ArgumentNullException();
+            }
 
-            // Convert coordinates from degrees, minutes, tenths of minutes
+            string upperCoords = coords.ToUpperInvariant();
+            if (upperCoords.Length != 8)
+            {
+                throw new ArgumentException("Latitude coordinates should be 8 characters. Given string was " + upperCoords.Length);
+            }
 
-            throw new NotImplementedException();
+            char direction = upperCoords[7];
+            if (upperCoords[7] != 'N' && upperCoords[7] != 'S')
+            {
+                throw new ArgumentException("Coordinates should end in N or S. Given string ended in " + upperCoords[7]);
+            }
+            if (upperCoords[4] != '.')
+            {
+                throw new ArgumentException("Coordinates should have '.' at index zero. Given string had " + upperCoords[4]);
+            }
+
+            // Count ambiguity and ensure no out of place spaces
+            Ambiguity = CountAmbiguity(upperCoords);
+            string modifiedCoords = EnforceAmbiguity(upperCoords, nEnforceAmbiguity);
+
+            // replace spaces with zeros
+            modifiedCoords = modifiedCoords.Replace(' ', '0');
+
+            // Break out strings and convert values
+            string degreesStr = modifiedCoords.Substring(0, 2);
+            string minutesStr = modifiedCoords.Substring(2, 2);
+            string hundMinutesStr = modifiedCoords.Substring(5, 2);
+
+            double degrees = double.Parse(degreesStr);
+            if (degrees < 00 || degrees > 90)
+            {
+                throw new ArgumentOutOfRangeException("Degrees must be in range [00,90]. Found: " + degrees + " in coord string " + modifiedCoords + " from given coord string " + coords);
+            }
+
+            double minutes = double.Parse(minutesStr);
+            double hundMinutes = double.Parse(hundMinutesStr);
+
+            double retval = degrees + (((minutes + (hundMinutes / 100)) / 60.0));
+
+            if (direction == 'S')
+            {
+                retval *= -1;
+            }
+
+            // limit significant figures
+            return Math.Round(retval, 4);
+        }
+
+        /// <summary>
+        /// Converts a string to have the given amount of ambiguity
+        /// </summary>
+        /// <param name="coords">String to convert</param>
+        /// <param name="nEnforceAmbiguity">Amount of ambiguity to emplace</param>
+        /// <returns></returns>
+        private string EnforceAmbiguity(string coords, int nEnforceAmbiguity)
+        {
+            if (coords == null)
+            {
+                throw new ArgumentNullException();
+            }
+            else if (nEnforceAmbiguity > coords.Length - 2)
+            {
+                throw new ArgumentOutOfRangeException("Only " + (coords.Length - 2) + " numbers can be filtered, but " + nEnforceAmbiguity + " were requested for string " + coords);
+            }
+
+            StringBuilder sb = new StringBuilder(coords);
+            int remainingAmbiguity = nEnforceAmbiguity;
+
+            for (int i = coords.Length - 2; i >= 0 && remainingAmbiguity > 0; --i)
+            {
+                if (i == coords.Length - 4)
+                {
+                    continue;
+                }
+
+                sb[i] = ' ';
+                --remainingAmbiguity;
+            }
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Counts the number of spaces from right to left and returns the number for ambiguity.
+        /// Also ensures there are no spaces left of a digit, which is an invalid APRS ambiguity syntax.
+        /// </summary>
+        /// <param name="coords">An APRS latitude or longitude string</param>
+        /// <returns></returns>
+        private int CountAmbiguity(string coords)
+        {
+            int ambiguity = 0;
+            bool foundDigit = false;
+            for (int i = coords.Length - 2; i >= 0; --i)
+            {
+                // skip the period
+                if (i == coords.Length - 4)
+                {
+                    continue;
+                }
+                else if (coords[i] == ' ')
+                {
+                    if (foundDigit)
+                    {
+                        throw new ArgumentException("Coordinates shoud only have spaces growing from right. Error on string: " + coords);
+                    }
+                    else
+                    {
+                        ++ambiguity;
+                    }
+                }
+                else
+                {
+                    foundDigit = true;
+                }
+            }
+
+            return ambiguity;
         }
 
         /// <summary>
         /// Decode the longitude section of the coordinate string
         /// </summary>
         /// <param name="coords">APRS encoded latitude coordinates</param>
+        /// <param name="nEnforceAmbiguity>Enforces a given amount of ambiguity</param>
         /// <returns>Decimal longitude</returns>
-        private double DecodeLongitude(string coords)
+        private double DecodeLongitude(string coords, int nEnforceAmbiguity)
         {
             // Convert coordinates from egrees, minutes, tenths of minutes
 

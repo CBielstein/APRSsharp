5b574a62b911f953a195dbb69aec9fc779a93474
Cameron Bielstein <ctbielstein@gmail.com>
2018-03-29 23:05:01 -0700

    Better comments in the KISSSpecialCharacters enum.

--- KissIt/KISSSpecialCharacters.cs
+++ KissIt/KISSSpecialCharacters.cs
@@ -4,11 +4,29 @@ using System.Text;
 
 namespace KissIt
 {
+    /// <summary>
+    /// Represents special characters in the KISS protocol
+    /// </summary>
     public enum KISSSpecialCharacters
     {
+        /// <summary>
+        /// Frame End
+        /// </summary>
         FEND = 0xC0,
+
+        /// <summary>
+        /// Frame Escape
+        /// </summary>
         FESC = 0xDB,
+
+        /// <summary>
+        /// Transposed Frame End
+        /// </summary>
         TFEND = 0xDC,
+
+        /// <summary>
+        /// Transposed Frame Escape
+        /// </summary>
         TFESC = 0xDD,
     }
 }

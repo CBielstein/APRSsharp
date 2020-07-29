e020ad134d1d4643c2f302dc4a5ba801123acaa1
eddywine <nyongesa.ed@gmail.com>
2020-07-29 07:33:55 +0300

    This is a draft PR to only take in input arguments and check AprsParsers

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AprsSharp.Parsers.Aprs;
+
+namespace ConsoleApplication.Aprs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {     
+          
+            Console.WriteLine("Enter the packet name ");
+            var packetName = Console.ReadLine();
+
+            Packet p = new Packet();
+      
+            p.DecodeInformationField (packetName);           
+      
+            Console.WriteLine($"\nHello, your packet is , {packetName}");
+                           
+        }
+    }
+}

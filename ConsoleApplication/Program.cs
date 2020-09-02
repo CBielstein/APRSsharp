ac0d667e8f5471e73ecac61eb48fca94b7af699b
eddywine <nyongesa.ed@gmail.com>
2020-09-03 02:37:47 +0300

    The modified version with style enforcement of console application

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -1,24 +1,27 @@
-﻿using System;
-using AprsSharp.Parsers.Aprs;
-
-namespace ConsoleApplication.Aprs
+﻿namespace AprsSharp.Applications.Console
 {
-    class Program
+    using System;
+    using AprsSharp.Parsers.Aprs;
+
+    /// <summary>
+    /// The public class that will be called when building the console application.
+    /// It is the main class that will have functionality of calling and decoding the packets.
+    /// </summary>
+    public class Program
     {
-        static void Main(string[] args)
-        {     
-          
-            Console.WriteLine("Enter the packet name ");
-            var packetName = Console.ReadLine();
-
-            Packet p = new Packet();      
-            p.DecodeInformationField (packetName);  
-
-            Timestamp ts = (Timestamp)p.timestamp;
-            Position pos = (Position)p.position;
-
-            Console.WriteLine($"\nHello, your packet name is, {p.comment}, at cordinates, {pos.Coordinates}, and time, {ts.dateTime.Hour}");
-                           
-        }
+        /// <summary>
+        /// Main method that takes in raw packet strings.
+        /// </summary>
+        /// <param name="args"> The input arguments for the program i.e packets which will be strings.</param>
+        public static void Main(string[] args)
+        {
+         Console.WriteLine("Enter the packet name ");
+         var packetName = Console.ReadLine();
+         Packet p = new Packet();
+         p.DecodeInformationField(packetName);
+         Timestamp ts = (Timestamp)p.timestamp;
+         Position pos = (Position)p.position;
+         Console.WriteLine($"\nHello, your packet name is, {p.comment}, at cordinates, {pos.Coordinates}, and time, {ts.dateTime.Hour}");
+         }
     }
 }

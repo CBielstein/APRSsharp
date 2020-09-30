8d23ef51626cd684e4ca3b7ced363c743cb509ca
eddywine <nyongesa.ed@gmail.com>
2020-10-01 00:23:06 +0300

    Modified version of timestamp and position

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -19,8 +19,8 @@
             var packetName = Console.ReadLine();
             Packet p = new Packet();
             p.DecodeInformationField(packetName);
-            Timestamp ts = (Timestamp)p.Timestamp;
-            Position pos = (Position)p.Position;
+            Timestamp ts = p.Timestamp;
+            Position pos = p.Position;
             Console.WriteLine($"\nHello, your packet name is, {p.Comment}, at cordinates, {pos.Coordinates}, and time, {ts.DateTime.Hour}");
          }
     }

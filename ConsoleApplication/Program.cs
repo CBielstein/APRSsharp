f4210290f86a5d6cecd1383a285a2409d855af1c
eddywine <nyongesa.ed@gmail.com>
2020-10-01 00:01:52 +0300

    Modified initial console application

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -15,13 +15,13 @@
         /// <param name="args"> The input arguments for the program i.e packets which will be strings.</param>
         public static void Main(string[] args)
         {
-         Console.WriteLine("Enter the packet name ");
-         var packetName = Console.ReadLine();
-         Packet p = new Packet();
-         p.DecodeInformationField(packetName);
-         Timestamp ts = (Timestamp)p.timestamp;
-         Position pos = (Position)p.position;
-         Console.WriteLine($"\nHello, your packet name is, {p.comment}, at cordinates, {pos.Coordinates}, and time, {ts.dateTime.Hour}");
+            Console.WriteLine("Enter the packet name ");
+            var packetName = Console.ReadLine();
+            Packet p = new Packet();
+            p.DecodeInformationField(packetName);
+            Timestamp ts = p.timestamp;
+            Position pos = p.position;
+            Console.WriteLine($"\nHello, your packet name is, {p.comment}, at cordinates, {pos.Coordinates}, and time, {ts.dateTime.Hour}");
          }
     }
 }

1fae9f46866980f3cf6b0d56c23837efec188800
Cameron Bielstein <ctbielstein@gmail.com>
2020-10-12 08:16:03 -0700

    Added unit test project for APRS IS

---
+++ test/AprsIsUnitTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+namespace AprsIsUnitTests
+{
+    using System;
+    using Xunit;
+
+    /// <summary>
+    /// Unit tests for APRS IS library.
+    /// </summary>
+    public class UnitTest1
+    {
+        /// <summary>
+        /// A test, I guess.
+        /// </summary>
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}

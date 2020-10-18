d816ac42caf156d1c519b25317fb8f7abeb8b2e3
Cameron Bielstein <ctbielstein@gmail.com>
2020-10-18 15:24:22 -0700

    Fixed issues in TimestampUnitTests.cs

--- test/AprsParserUnitTests/TimestampUnitTests.cs
+++ test/AprsParserUnitTests/TimestampUnitTests.cs
@@ -1,98 +1,91 @@
-﻿using System;
-using Xunit;
-using AprsSharp.Parsers.Aprs;
-
-namespace AprsSharpUnitTests.Parsers.Aprs
+﻿namespace AprsSharpUnitTests.Parsers.Aprs
 {
+    using System;
+    using AprsSharp.Parsers.Aprs;
+    using Xunit;
+
+    /// <summary>
+    /// Test Timestamp code.
+    /// </summary>
     public class TimestampUnitTests
     {
         /// <summary>
-        /// Pass a day that is out of the bounds of [1,31] and expect an exception
+        /// Pass a day that is out of the bounds of [1,31] and expect an exception.
         /// </summary>
         [Fact]
         public void FindCorrectYearAndMonth_DayOutOfRange()
         {
             Timestamp ts = new Timestamp();
-            
-            int outYear = 0;
-            int outDay = 0;
-            
+
             try
             {
-                ts.FindCorrectYearAndMonth(32, DateTime.Now, out outYear, out outDay);
-                
+                ts.FindCorrectYearAndMonth(32, DateTime.Now, out _, out _);
+
                 // Should not reach here as an ArgumentOutOfRangeException should be thrown
                 Assert.True(false);
             }
             catch (Exception e)
             {
-                
                 Assert.IsType<ArgumentOutOfRangeException>(e);
             }
         }
 
         /// <summary>
-        /// Ensures we find the correct month & year when we're in the same month.
+        /// Ensures we find the correct month and year when we're in the same month.
         /// </summary>
         [Fact]
         public void FindCorrectYearAndMonth_SameMonth()
         {
             Timestamp ts = new Timestamp();
-            
+
             int expectedYear = 2016;
             int expectedMonth = 10;
-            DateTime MidMonth = new DateTime(expectedYear, expectedMonth, 24);
-            int year = 0;
-            int month = 0;
+            DateTime midMonth = new DateTime(expectedYear, expectedMonth, 24);
 
-            ts.FindCorrectYearAndMonth(7, MidMonth, out year, out month);
+            ts.FindCorrectYearAndMonth(7, midMonth, out int year, out int month);
 
             Assert.Equal(expectedYear, year);
             Assert.Equal(expectedMonth, month);
         }
 
         /// <summary>
-        /// Ensures we find the correct month & year when the day is from last month.
+        /// Ensures we find the correct month and year when the day is from last month.
         /// </summary>
         [Fact]
         public void FindCorrectYearAndMonth_PreviousMonth()
         {
             Timestamp ts = new Timestamp();
-           
+
             int expectedYear = 2016;
             int expectedMonth = 9;
-            DateTime MidMonth = new DateTime(expectedYear, 10, 24);
-            int year = 0;
-            int month = 0;
+            DateTime midMonth = new DateTime(expectedYear, 10, 24);
 
-            ts.FindCorrectYearAndMonth(25, MidMonth, out year,  out month);
+            ts.FindCorrectYearAndMonth(25, midMonth, out int year, out int month);
 
             Assert.Equal(expectedYear, year);
             Assert.Equal(expectedMonth, month);
         }
 
         /// <summary>
-        /// Ensures we find the correct month & year when the day is from last year.
+        /// Ensures we find the correct month and year when the day is from last year.
         /// </summary>
         [Fact]
         public void FindCorrectYearAndMonth_PreviousYear()
         {
             Timestamp ts = new Timestamp();
-            
+
             int expectedYear = 2015;
             int expectedMonth = 12;
-            DateTime MidMonth = new DateTime(2016, 1, 1);
-            int month = 0;
-            int year = 0;
+            DateTime midMonth = new DateTime(2016, 1, 1);
 
-            ts.FindCorrectYearAndMonth(31, MidMonth, out year, out month);
+            ts.FindCorrectYearAndMonth(31, midMonth, out int year, out int month);
 
             Assert.Equal(expectedYear, year);
             Assert.Equal(expectedMonth, month);
         }
 
         /// <summary>
-        /// Ensures we find the correct month & year when the day is from two months ago.
+        /// Ensures we find the correct month and year when the day is from two months ago.
         /// Skips 2/30, since that day doesn't exist.
         /// </summary>
         [Fact]
@@ -102,18 +95,16 @@ namespace AprsSharpUnitTests.Parsers.Aprs
 
             int expectedYear = 2016;
             int expectedMonth = 1;
-            DateTime MidMonth = new DateTime(expectedYear, 3, 1);
-            int year = 0;
-            int month = 0;
+            DateTime midMonth = new DateTime(expectedYear, 3, 1);
 
-            ts.FindCorrectYearAndMonth(30, MidMonth, out year, out month);
+            ts.FindCorrectYearAndMonth(30, midMonth, out int year, out int month);
 
             Assert.Equal(expectedYear,  year);
             Assert.Equal(expectedMonth, month);
         }
 
         /// <summary>
-        /// Ensures we find the correct month & year when the day is from two months ago.
+        /// Ensures we find the correct month and year when the day is from two months ago.
         /// Skips 2/29, since that day doesn't exist in 2015.
         /// </summary>
         [Fact]
@@ -124,39 +115,35 @@ namespace AprsSharpUnitTests.Parsers.Aprs
             int expectedYear = 2015;
             int expectedMonth = 1;
 
-            DateTime MidMonth = new DateTime(expectedYear, 3, 1);
-            int year = 0;
-            int month = 0;
+            DateTime midMonth = new DateTime(expectedYear, 3, 1);
 
-            ts.FindCorrectYearAndMonth(29, MidMonth, out year, out month);
+            ts.FindCorrectYearAndMonth(29, midMonth, out int year, out int month);
 
             Assert.Equal(expectedYear, year);
             Assert.Equal(expectedMonth, month);
         }
 
         /// <summary>
-        /// Ensures we find the correct month & year when the day is from two months ago.
+        /// Ensures we find the correct month and year when the day is from two months ago.
         /// Does NOT skip 2/29, since that day DOES exist in 2016.
         /// </summary>
         [Fact]
         public void FindCorrectYearAndMonth_LeapYear()
         {
             Timestamp ts = new Timestamp();
- 
+
             int expectedYear = 2016;
             int expectedMonth = 2;
-            DateTime MidMonth = new DateTime(expectedYear, 3, 1);
-            int year = 0;
-            int month = 0;
+            DateTime midMonth = new DateTime(expectedYear, 3, 1);
 
-            ts.FindCorrectYearAndMonth(29, MidMonth, out year,  out month );
+            ts.FindCorrectYearAndMonth(29, midMonth, out int year, out int month);
 
             Assert.Equal(expectedYear, year);
             Assert.Equal(expectedMonth, month);
         }
 
         /// <summary>
-        /// Tests example given in the APRS101.pdf document for zulu time
+        /// Tests example given in the APRS101.pdf document for zulu time.
         /// </summary>
         [Fact]
         public void DHMZuluTimeFromSpec()
@@ -171,7 +158,7 @@ namespace AprsSharpUnitTests.Parsers.Aprs
         }
 
         /// <summary>
-        /// Tests example given in the APRS101.pdf document for local time
+        /// Tests example given in the APRS101.pdf document for local time.
         /// </summary>
         [Fact]
         public void DHMNotZuluTimeFromSpec()
@@ -187,26 +174,24 @@ namespace AprsSharpUnitTests.Parsers.Aprs
 
         /// <summary>
         /// Test FindCorrectDayMonthAndYear from an HMS packet perspective
-        /// assuming the packet is from the same day using local
+        /// assuming the packet is from the same day using local.
         /// </summary>
         [Fact]
         public void FindCorrectDayMonthAndYear_SameDayLocal()
         {
             Timestamp ts = new Timestamp();
-            
+
             DateTime packet = new DateTime(2016, 11, 18, 22, 48, 16, DateTimeKind.Local);
             DateTime hint = packet.AddHours(1);
-            int day = 0;
-            int month = 0;
-            int year = 0;
 
-            ts.FindCorrectDayMonthAndYear(packet.Hour,
-            packet.Minute,
-            packet.Second,
-            hint,
-            out day,
-            out month,
-            out year);
+            ts.FindCorrectDayMonthAndYear(
+                packet.Hour,
+                packet.Minute,
+                packet.Second,
+                hint,
+                out int day,
+                out int month,
+                out int year);
 
             Assert.Equal(packet.Day, day);
             Assert.Equal(packet.Month, month);
@@ -215,7 +200,7 @@ namespace AprsSharpUnitTests.Parsers.Aprs
 
         /// <summary>
         /// Test FindCorrectDayMonthAndYear from an HMS packet perspective
-        /// assuming the packet is from yesterday using zulu
+        /// assuming the packet is from yesterday using zulu.
         /// </summary>
         [Fact]
         public void FindCorrectDayMonthAndYear_YesterdayZulu()
@@ -224,17 +209,15 @@ namespace AprsSharpUnitTests.Parsers.Aprs
 
             DateTime packet = new DateTime(2016, 11, 18, 22, 48, 16, DateTimeKind.Utc);
             DateTime hint = packet.AddHours(-2);
-            int day = 0;
-            int month = 0;
-            int year = 0;
 
-            ts.FindCorrectDayMonthAndYear(packet.Hour,
+            ts.FindCorrectDayMonthAndYear(
+                packet.Hour,
                 packet.Minute,
                 packet.Second,
                 hint,
-                out day,
-                out month,
-                out year);
+                out int day,
+                out int month,
+                out int year);
 
             Assert.Equal(packet.Day - 1, day);
             Assert.Equal(packet.Month, month);
@@ -243,7 +226,7 @@ namespace AprsSharpUnitTests.Parsers.Aprs
 
         /// <summary>
         /// Test FindCorrectDayMonthAndYear from an HMS packet perspective
-        /// with the packet coming from 3 minutes before hint
+        /// with the packet coming from 3 minutes before hint.
         /// </summary>
         [Fact]
         public void FindCorrectDayMonthAndYear_3MinutesAhead()
@@ -252,17 +235,15 @@ namespace AprsSharpUnitTests.Parsers.Aprs
 
             DateTime packet = new DateTime(2017, 1, 1, 0, 1, 1, DateTimeKind.Utc);
             DateTime hint = packet.AddMinutes(-3);
-            int day = 0;
-            int month = 0;
-            int year = 0;
 
-            ts.FindCorrectDayMonthAndYear(packet.Hour,
+            ts.FindCorrectDayMonthAndYear(
+                packet.Hour,
                 packet.Minute,
                 packet.Second,
                 hint,
-                out day,
-                out month,
-                out year);
+                out int day,
+                out int month,
+                out int year);
 
             Assert.Equal(hint.Day, day);
             Assert.Equal(hint.Month, month);
@@ -270,7 +251,7 @@ namespace AprsSharpUnitTests.Parsers.Aprs
         }
 
         /// <summary>
-        /// Test HMS with example from APRS101.pdf
+        /// Test HMS with example from APRS101.pdf.
         /// </summary>
         [Fact]
         public void HMSTestFromSpec()
@@ -284,29 +265,29 @@ namespace AprsSharpUnitTests.Parsers.Aprs
         }
 
         /// <summary>
-        /// Test FindCorrectYear's 5 minute drift logic
+        /// Test FindCorrectYear's 5 minute drift logic.
         /// </summary>
         [Fact]
         public void FindCorrectYear_3MinutesAhead()
         {
             Timestamp ts = new Timestamp();
-  
+
             DateTime packet = new DateTime(2017, 1, 1, 0, 1, 1, DateTimeKind.Utc);
             DateTime hint = packet.AddMinutes(-3);
-            int year = 0;
 
-            ts.FindCorrectYear(packet.Month,
+            ts.FindCorrectYear(
+                packet.Month,
                 packet.Day,
                 packet.Hour,
                 packet.Minute,
                 hint,
-                out year);
+                out int year);
 
             Assert.Equal(hint.Year, year);
         }
 
         /// <summary>
-        /// Test FindCorrectYear with a packet from earlier this year
+        /// Test FindCorrectYear with a packet from earlier this year.
         /// </summary>
         [Fact]
         public void FindCorrectYear_ThisYear()
@@ -315,20 +296,20 @@ namespace AprsSharpUnitTests.Parsers.Aprs
 
             DateTime packet = new DateTime(2016, 11, 18, 0, 1, 1, DateTimeKind.Utc);
             DateTime hint = packet.AddMonths(1);
-            int year = 0;
 
-            ts.FindCorrectYear( packet.Month,
+            ts.FindCorrectYear(
+                packet.Month,
                 packet.Day,
                 packet.Hour,
                 packet.Minute,
                 hint,
-                out year);
+                out int year);
 
             Assert.Equal(packet.Year, year);
         }
 
         /// <summary>
-        /// Test FindCorrectYear with a packet from last year
+        /// Test FindCorrectYear with a packet from last year.
         /// </summary>
         [Fact]
         public void FindCorrectYear_LastYear()
@@ -337,20 +318,20 @@ namespace AprsSharpUnitTests.Parsers.Aprs
 
             DateTime packet = new DateTime(2016, 11, 18, 0, 1, 1, DateTimeKind.Utc);
             DateTime hint = packet.AddMonths(3);
-            int year = 0;
 
-            ts.FindCorrectYear(packet.Month,
+            ts.FindCorrectYear(
+                packet.Month,
                 packet.Day,
                 packet.Hour,
                 packet.Minute,
                 hint,
-                out year);
-            
+                out int year);
+
             Assert.Equal(packet.Year, year);
         }
 
         /// <summary>
-        /// Test FindCorrectYear leap year edition
+        /// Test FindCorrectYear leap year edition.
         /// </summary>
         [Fact]
         public void FindCorrectYear_LeapYear()
@@ -359,20 +340,20 @@ namespace AprsSharpUnitTests.Parsers.Aprs
 
             DateTime packet = new DateTime(2015, 2, 28, 0, 1, 1, DateTimeKind.Utc);
             DateTime hint = packet.AddMonths(1);
-            int year = 0;
-            
-            ts.FindCorrectYear(packet.Month,
+
+            ts.FindCorrectYear(
+                packet.Month,
                 packet.Day + 1,
                 packet.Hour,
                 packet.Minute,
                 hint,
-                out year);
-            
+                out int year);
+
             Assert.Equal(2012, year);
         }
 
         /// <summary>
-        /// Test MDHM with the example from the APRS spec
+        /// Test MDHM with the example from the APRS spec.
         /// </summary>
         [Fact]
         public void MDHMTestFromSpec()
@@ -387,7 +368,7 @@ namespace AprsSharpUnitTests.Parsers.Aprs
         }
 
         /// <summary>
-        /// Encodes in DHM zulu time with the example from the APRS spec
+        /// Encodes in DHM zulu time with the example from the APRS spec.
         /// </summary>
         [Fact]
         public void EncodeDHMZuluTestFromSpec()
@@ -399,7 +380,7 @@ namespace AprsSharpUnitTests.Parsers.Aprs
         }
 
         /// <summary>
-        /// Encodes in DHM local time with the example from the APRS spec
+        /// Encodes in DHM local time with the example from the APRS spec.
         /// </summary>
         [Fact]
         public void EncodeDHMLocalTestFromSpec()

fd74fb3a55b0eaf17465c9ffd54a6878cea71750
eddywine <nyongesa.ed@gmail.com>
2020-12-15 20:12:35 +0300

    Implementation of delegate

--- src/AprsISLibrary/AprsIsConnection.cs
+++ src/AprsISLibrary/AprsIsConnection.cs
@@ -44,7 +44,6 @@
             // Variables string callsign = "N0CALL
             string callsign = "NOCALL"; // Radius of 50km of Seattle's Space Needle
             string password = "-1";
-            Console.WriteLine("empty strings input");
             await Receive(callsign, password).ConfigureAwait(false);
         }
 
@@ -54,14 +53,15 @@
         /// <param name="callsign">Specifying the different strings.</param>
         /// <param name="password">Specifying the password input strings.</param>
         /// <returns>An async task.</returns>
-        public async Task Receive(string callsign, string password)
+        public async Task Receive(string? callsign, string? password)
         {
             // Variables string callsign = "N0CALL
+            callsign = callsign ?? "N0CALL";
+            password = password ?? "-1";
             string filter = "filter r/50.5039/4.4699/50"; // Radius of Belgium's Space Needle
             string authString = $"user {callsign} pass {password} vers AprsSharp 0.1 {filter}";
             string server = "rotate.aprs2.net";
             bool authenticated = false;
-            Console.WriteLine("inputs");
 
             // Open connection
             tcpConnection.Connect(server, 14580);

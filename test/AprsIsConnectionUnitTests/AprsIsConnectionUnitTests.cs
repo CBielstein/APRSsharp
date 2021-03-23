6ed0667387a96909440bf069acfdd0a030e67c9c
Cameron Bielstein <ctbielstein@gmail.com>
2021-03-22 22:23:52 -0700

    Added test project.

---
+++ test/AprsIsConnectionUnitTests/AprsIsConnectionUnitTests.cs
@@ -0,0 +1,13 @@
+namespace AprsSharpUnitTests.Connections.AprsIs
+{
+    using Xunit;
+
+    public class AprsIsConnectionUnitTests
+    {
+        [Fact]
+        public void TestDiconnect()
+        {
+
+        }
+    }
+}

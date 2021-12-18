81c9dba11fd15cde1bce9babcc5eb344ff703819
Cameron Bielstein <ctbielstein@gmail.com>
2021-12-17 22:53:29 -0800

    Fix indentation.

--- src/AprsParser/TimestampType.cs
+++ src/AprsParser/TimestampType.cs
@@ -1,33 +1,33 @@
 namespace AprsSharp.Parsers.Aprs
 {
+    /// <summary>
+    /// Represents the type of APRS timestamp.
+    /// </summary>
+    public enum TimestampType
+    {
         /// <summary>
-        /// Represents the type of APRS timestamp.
+        /// Days/Hours/Minutes zulu
         /// </summary>
-        public enum TimestampType
-        {
-            /// <summary>
-            /// Days/Hours/Minutes zulu
-            /// </summary>
-            DHMz,
+        DHMz,
 
-            /// <summary>
-            /// Days/Hours/Minutes local
-            /// </summary>
-            DHMl,
+        /// <summary>
+        /// Days/Hours/Minutes local
+        /// </summary>
+        DHMl,
 
-            /// <summary>
-            /// Hours/Minutes/Seconds (always zulu)
-            /// </summary>
-            HMS,
+        /// <summary>
+        /// Hours/Minutes/Seconds (always zulu)
+        /// </summary>
+        HMS,
 
-            /// <summary>
-            /// Hours/Minutes/Seconds (always zulu)
-            /// </summary>
-            MDHM,
+        /// <summary>
+        /// Hours/Minutes/Seconds (always zulu)
+        /// </summary>
+        MDHM,
 
-            /// <summary>
-            /// Not a decoded timestamp
-            /// </summary>
-            NotDecoded,
-        }
+        /// <summary>
+        /// Not a decoded timestamp
+        /// </summary>
+        NotDecoded,
+    }
 }

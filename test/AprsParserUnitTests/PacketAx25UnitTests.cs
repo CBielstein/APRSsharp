8d4c21edfe9f8d51931206c7da61cb141c541551
Cameron Bielstein <ctbielstein@gmail.com>
2023-07-25 20:51:50 -0700

    Add roundtrip AX.25 encode/decode test.

---
+++ test/AprsParserUnitTests/PacketAx25UnitTests.cs
@@ -0,0 +1,42 @@
+﻿namespace AprsSharpUnitTests.Parsers.Aprs;
+
+using System;
+using AprsSharp.Parsers.Aprs;
+using Xunit;
+
+/// <summary>
+/// Tests <see cref="Packet"/> code for encoding and decoding AX.25 packet format.
+/// </summary>
+public class PacketAx25UnitTests
+{
+    /// <summary>
+    /// Tests a full roundtrip encode then decode in AX.25.
+    /// </summary>
+    [Fact]
+    public void RoundTripEncodeDecode()
+    {
+        var sender = "N0CALL";
+        var destination = "N0NE";
+        var path = new string[2] { "WIDE2-2", "WIDE1-1" };
+        var info = new StatusInfo(new Timestamp(DateTime.UtcNow), "Testing 1 2 3!");
+
+        var packet = new Packet(sender, destination, path, info);
+
+        var encoded = packet.EncodeAx25();
+
+        var decodedPacket = new Packet(encoded);
+
+        Assert.Equal(sender, decodedPacket.Sender);
+        Assert.Equal(destination, decodedPacket.Destination);
+        Assert.Equal(path, decodedPacket.Path);
+
+        Assert.IsType<StatusInfo>(decodedPacket.InfoField);
+        var si = (StatusInfo)decodedPacket.InfoField ?? throw new Exception();
+        Assert.Equal(DateTimeKind.Utc, si.Timestamp?.DateTime.Kind);
+        Assert.Equal(info.Timestamp?.DateTime.Day, si.Timestamp?.DateTime.Day);
+        Assert.Equal(info.Timestamp?.DateTime.Hour, si.Timestamp?.DateTime.Hour);
+        Assert.Equal(info.Timestamp?.DateTime.Minute, si.Timestamp?.DateTime.Minute);
+        Assert.Equal(info.Comment, si.Comment);
+        Assert.Equal(info.Position?.Coordinates, si.Position?.Coordinates);
+    }
+}

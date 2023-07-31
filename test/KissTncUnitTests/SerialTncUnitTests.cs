3154f2ee131bfe915f2219f7a1af92d4025a7ef2
Cameron Bielstein <ctbielstein@gmail.com>
2023-07-30 18:48:41 -0700

    Update SerialTNC class to SerialTnc

--- test/KissTncUnitTests/SerialTncUnitTests.cs
+++ test/KissTncUnitTests/SerialTncUnitTests.cs
@@ -5,19 +5,19 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 
 /// <summary>
-/// Test <see cref="SerialTNC"/> class.
+/// Test <see cref="SerialTnc"/> class.
 /// </summary>
 [TestClass]
-public class SerialTNCUnitTests : BaseTNCUnitTests<SerialTNC>
+public class SerialTncUnitTests : BaseTNCUnitTests<SerialTnc>
 {
     /// <inheritdoc/>
-    public override SerialTNC BuildTestTnc()
+    public override SerialTnc BuildTestTnc()
     {
         var mockPort = new Mock<ISerialConnection>();
         mockPort.SetupGet(mock => mock.IsOpen).Returns(false);
         mockPort.Setup(mock => mock.Open());
         mockPort.Setup(mock => mock.Write(It.IsAny<byte[]>(), It.IsAny<int>(), It.IsAny<int>()));
 
-        return new SerialTNC(mockPort.Object, 0);
+        return new SerialTnc(mockPort.Object, 0);
     }
 }
